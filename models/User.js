e1013aa3d03f0e7d17941bced3a0e0a34a1edc64
brt9029 <brt9029@gmail.com>
2021-12-31 13:50:42 -0600

    user model created with routing

---
+++ models/User.js
@@ -0,0 +1,60 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create user model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        // id column
+        id: {
+            // sequelize datatypes opbject provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent to NOT NULL
+            allowNull: false,
+            // set as primary key
+            primaryKey: true,
+            // auto increment
+            autoIncrement: true
+        },
+        // username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // do not allow any duplicate email values on this table
+            unique: true,
+            // if allowNull is set to false, we can run our data through validators before creating the table data 
+            validate: {
+                isEmail: true
+            }
+        },
+        // password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // make password min for characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel=casing
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;

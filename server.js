12032c8f4575a10539045433a1751a6531dd4cec
brt9029 <brt9029@gmail.com>
2022-09-19 17:52:00 -0700

    refactored links to be shortened to domain only when posting, pluralization is properly appended, dates are formatted

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const sequelize = require('./config/connection');
 const session = require ('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const helpers = require('./utils/helpers');
+
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -22,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
